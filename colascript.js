d863725ba003a1e4adc5137b76fd69d690ff3ce0
CoLaMusic <60479738+CoLaMusic@users.noreply.github.com>
2023-01-15 09:55:47 +0200

    Update colascript.js

--- colascript.js
+++ colascript.js
@@ -110,7 +110,7 @@
 
             pcb_img = listModelsSimple[y].img;
 
-            box = `<iframe src="${pcb_img}" width="100%" height="1050">`;
+            box = `<iframe src="${pcb_img}" width="721" height="242" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" loading="lazy">`;
                   
             content.innerHTML += box;
 
