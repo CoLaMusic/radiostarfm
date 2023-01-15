db07d8000e27f32a99b8f858a44456d30cd91144
CoLaMusic <60479738+CoLaMusic@users.noreply.github.com>
2023-01-15 02:03:58 +0200

    Update cola.js

--- cola.js
+++ cola.js
@@ -9,7 +9,19 @@ let modelos = [
       },
       {
             img: 'https://program.radiostarfmromania.com/timetable_public.php',
-            type: 'pcback'
+            type: 'program'
+      },
+     {
+            img: 'https://program.radiostarfmromania.com/dedicatii',
+            type: 'dedicatii'
+      },
+      {
+            img: 'https://tvhd-online.com',
+            type: 'programetv'
+      },
+      {
+            img: 'https://www.eastrolog.ro/webmasteri/horoscop/v31hz72aD01.php',
+            type: 'horoscop'
       },
       
 ]
